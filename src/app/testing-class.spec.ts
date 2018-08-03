e3d35f18b737a1b1f38660dc83d486ade1819fba
EduWeb.pl <eduweb@eduweb.pl>
2018-08-03 17:18:39 +0200

    2.3. Jasmine i asercje testowe - finished

---
+++ src/app/testing-class.spec.ts
@@ -0,0 +1,15 @@
+import { TestingClass } from './testing-class';
+
+describe('TestingClass', () => {
+
+  it('should create an instance', () => {
+
+    expect(new TestingClass()).toBeTruthy()
+  });
+
+  it('should test that true is truthy', ()=>{
+    
+    expect(true).toBeTruthy()
+  })
+
+});
