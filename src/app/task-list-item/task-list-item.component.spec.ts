fff3fef169626e6d081dbd38212a8bc4cdd7b46b
Mateusz Kulesza <eduwebgit@gmail.com>
2018-08-28 15:27:58 +0200

    2.8. Testowanie formularzy - finished

--- src/app/task-list-item/task-list-item.component.spec.ts
+++ src/app/task-list-item/task-list-item.component.spec.ts
@@ -38,7 +38,7 @@ describe("TaskListItemComponent", () => {
   });
 
   it('should switch to edit mode when "edit" button is clicked', () => {
-    const editSpy = spyOn(component, "edit").and.callThrough()
+    const editSpy = spyOn(component, "edit").and.callThrough();
     const button = fixture.debugElement.query(By.css(".edit-button"));
 
     button.triggerEventHandler("click", {});
@@ -46,5 +46,25 @@ describe("TaskListItemComponent", () => {
     expect(editSpy).toHaveBeenCalled();
     expect(component.editMode).toEqual(true);
   });
-  
+
+  it("should show task name inside input", () => {
+    const input = fixture.debugElement.query(By.css(".task-name-input"));
+    expect((input.nativeElement as HTMLInputElement).value).toEqual(
+      component.message
+    );
+  });
+
+  it("should update task name when input value changes", () => {
+    const inputElem = fixture.debugElement.query(By.css(".task-name-input"));
+    const elem = inputElem.nativeElement as HTMLInputElement;
+
+    elem.value = "Changed value!";
+    // elem.dispatchEvent(new Event("input"));
+
+    inputElem.triggerEventHandler("input", {
+      target: elem
+    });
+
+    expect(component.message).toEqual("Changed value!");
+  });
 });
