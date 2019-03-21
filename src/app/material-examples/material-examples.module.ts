7d20b439fa26dde09dd728346bb2b82f69f7f749
Mateusz Kulesza <mateusz@kulesza.biz>
2019-03-21 16:32:08 +0100

    3.3. Ikony i wskaźniki - finished

--- src/app/material-examples/material-examples.module.ts
+++ src/app/material-examples/material-examples.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MaterialExamplesRoutingModule } from './material-examples-routing.module';
 import { IndicatorsExampleComponent } from './pages/indicators-example/indicators-example.component';
 
-import { } from '@angular/material'
+import { MatIconModule, MatBadgeModule, MatTooltipModule } from '@angular/material'
 
 @NgModule({
     declarations: [IndicatorsExampleComponent],
     imports: [
         CommonModule,
         MaterialExamplesRoutingModule,
+        MatIconModule,
+        MatBadgeModule,
+        MatTooltipModule
     ]
 })
 export class MaterialExamplesModule { }
